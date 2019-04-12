b2f451fbcc8f0d43a6ab6ba99cb686ff77b4ab50
Yuriy Naydenov <ebceu44@gmail.com>
2019-04-12 17:31:37 +0300

    keeperStore

--- frontend/src/store/RootStore.ts
+++ frontend/src/store/RootStore.ts
@@ -1,10 +1,13 @@
 import AppStore from './AppStore'
+import KeeperStore from './KeeperStore'
 
 export default class RootStore {
-    public appStore: AppStore
+  public appStore: AppStore
+  public keeperStore: KeeperStore
 
-    constructor(){
-        this.appStore = new AppStore(this)
-    }
+  constructor() {
+    this.appStore = new AppStore(this)
+    this.keeperStore = new KeeperStore(this)
+  }
 }
 
