2fd3507263bac8fb674f681280c1fe69a330d9dd
Arsenii Pechenkin <arspechen@gmail.com>
2019-04-14 00:23:28 +0300

    add fix to get shipment details

--- backend/controllers/shipmentsController.js
+++ backend/controllers/shipmentsController.js
@@ -9,14 +9,21 @@ metricRep = require('../repository/metricsRepository');
 CreateShipmentModel = require('../models/api/shipment/CreateShipmentRequest');
 GetShipmentModel = require('../models/api/shipment/GetShipmentResponce');
 
-async function create (req, res) {
+async function create(req, res) {
     console.log("api shipment create");
     try {
         var shipment = req.body;
-        await shipmentsRep.createShipment(shipment);
+        shipment.createDate = Date.Now;
+        var newShipment = await shipmentsRep.createShipment(shipment);
+        if(shipment.goods){
+            shipment.goods.forEach(async (entry) => {
+                entry.shipmentId = newShipment.id
+                await goodsRep.create(entry);
+            });
+        }
         res.json({
-                message: 'New shipment created!',
-                data: shipment
+            message: 'New shipment created!',
+            data: shipment
         });
     }
     catch (e) {
@@ -25,9 +32,9 @@ async function create (req, res) {
     }
 };
 
-async function index (req, res) {
-    try { 
-    console.log("api shipment index");
+async function index(req, res) {
+    try {
+        console.log("api shipment index");
         var shipments = await shipmentsRep.getShipments();
         res.json({
             status: "success",
@@ -41,10 +48,9 @@ async function index (req, res) {
     }
 };
 
-async function allRecived (req,res) {
+async function allRecived(req, res) {
     try {
-        
-    console.log("api shipment allRecived");
+        console.log("api shipment allRecived");
         var userId = req.params.user_id
         var shipments = await shipmentsRep.getShipmentsByReciverId(userId);
         res.json({
@@ -59,10 +65,9 @@ async function allRecived (req,res) {
     }
 }
 
-async function allSend (req,res) {
+async function allSend(req, res) {
     try {
-        
-    console.log("api shipment allSend");
+        console.log("api shipment allSend");
         var userId = req.params.user_id
         var shipments = await shipmentsRep.getShipmentsBySenderId(userId);
         res.json({
@@ -77,10 +82,10 @@ async function allSend (req,res) {
     }
 }
 
-async function allCarier (req,res) {
+async function allCarier(req, res) {
     try {
-       
-    console.log("api shipment allCarier");
+
+        console.log("api shipment allCarier");
         var userId = req.params.user_id
         var shipments = await shipmentsRep.getShipmentsByCarierId(userId);
         res.json({
@@ -95,12 +100,12 @@ async function allCarier (req,res) {
     }
 }
 
-async function view (req, res) {
+async function view(req, res) {
     try {
-        
+
         console.log("api shipment view");
         var shipmentId = req.params.shipment_id
-        var shipment = await shipmentsRep.findById(shipmentId);
+        let shipment = await shipmentsRep.findById(shipmentId);
         shipment.goods = await goodsRep.findByShipmentId(shipmentId);
         shipment.claims = await claimRep.findByShipmentId(shipmentId);
         shipment.extraInfo = await extraInfoRep.findByShipmentId(shipmentId);
@@ -108,7 +113,7 @@ async function view (req, res) {
         shipment.metricData = await metricRep.findByDeviceId(shipment.device);
         res.json({
             status: "success",
-            message: "Shipment details successfully",
+            message: "Shipment details successfully1",
             data: shipment
         });
     }
@@ -118,10 +123,10 @@ async function view (req, res) {
     }
 };
 
-async function transfer (req, res) {
+async function transfer(req, res) {
     try {
-        
-    console.log("api shipment transfer");
+
+        console.log("api shipment transfer");
         var transfer = req.body
         var shipment = await shipmentsRep.findById(transfer.shipmentId);
         shipment.countryFrom = transfer.from;
@@ -140,10 +145,10 @@ async function transfer (req, res) {
     }
 };
 
-async function changeStatus (req, res) {
+async function changeStatus(req, res) {
     try {
-        
-    console.log("api shipment changeStatus");
+
+        console.log("api shipment changeStatus");
         var transfer = req.body
         var shipment = await shipmentsRep.findById(transfer.shipmentId);
         shipment.status = transfer.status;
--- backend/repository/shipmentRepository.js
+++ backend/repository/shipmentRepository.js
@@ -11,48 +11,17 @@ const pool = new Pool({
 
 
 
-async function createShipment (shipment) {
+async function createShipment(shipment) {
   console.log("db createShipment");
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO shipments (sender, recipient, countryfrom, countryto, departuredate, policeid, carrier, title, createdate, status, arrivaldate, device) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)', 
-        [
-          shipment.sender, 
-          shipment.recipient, 
-          shipment.from, 
-          shipment.to, 
-          shipment.departureDate, 
-          shipment.PoliciId,
-          shipment.carrier, 
-          shipment.title, 
-          shipment.createDate,
-          shipment.status,
-          shipment.arrivalDate,
-          shipment.device
-        ], (error, results) => {
-        if (error) {
-          console.log(error);
-          reject(error);
-        }
-        if(results)
-            resolve(results.rows);
-          resolve();
-      })
-    })
-  }
-
-
-async function update (shipment) {
-  console.log("db update shipment");
-return new Promise((resolve, reject) => {
-  pool.query('UPDATE shipment SET sender = $2, recipient = $3, countryfrom = $4, countryto = $5, departuredate = $6, policeid = $7, carrier = $8, title = $9, createdate = $10, status = $11, arrivaldate = $12, device = $13 WHERE id = $1',
+  return new Promise((resolve, reject) => {
+    pool.query('INSERT INTO shipments (sender, recipient, countryfrom, countryto, departuredate, policyid, carrier, title, createdate, status, arrivaldate, device) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) RETURNING *',
       [
-        shipment.id,
         shipment.sender,
         shipment.recipient,
         shipment.from,
         shipment.to,
         shipment.departureDate,
-        shipment.PoliciId,
+        shipment.PolicyId,
         shipment.carrier,
         shipment.title,
         shipment.createDate,
@@ -64,11 +33,42 @@ return new Promise((resolve, reject) => {
           console.log(error);
           reject(error);
         }
-        if(results)
-            resolve(results.rows);
-          resolve();
+        if (results)
+          resolve(results.rows[0]);
+        resolve();
       })
-})
+  })
+}
+
+
+async function update(shipment) {
+  console.log("db update shipment");
+  return new Promise((resolve, reject) => {
+    pool.query('UPDATE shipment SET sender = $2, recipient = $3, countryfrom = $4, countryto = $5, departuredate = $6, policyid = $7, carrier = $8, title = $9, createdate = $10, status = $11, arrivaldate = $12, device = $13 WHERE id = $1',
+      [
+        shipment.id,
+        shipment.sender,
+        shipment.recipient,
+        shipment.from,
+        shipment.to,
+        shipment.departureDate,
+        shipment.PolicyId,
+        shipment.carrier,
+        shipment.title,
+        shipment.createDate,
+        shipment.status,
+        shipment.arrivalDate,
+        shipment.device
+      ], (error, results) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        }
+        if (results)
+          resolve(results.rows);
+        resolve();
+      })
+  })
 }
 
 async function getShipments() {
@@ -78,74 +78,74 @@ async function getShipments() {
       if (error) {
         reject(error);
       }
-      if(results)
-            resolve(results.rows);
-          resolve();
+      if (results)
+        resolve(results.rows);
+      resolve();
     });
   })
 }
 
-async function getShipmentsBySenderId (userId) {
+async function getShipmentsBySenderId(userId) {
   console.log("db getShipmentsBySenderId");
   return new Promise((resolve, reject) => {
-    pool.query('SELECT * FROM shipments WHERE sender = $1  ORDER BY id ASC', [userId],  (error, results) => {
+    pool.query('SELECT * FROM shipments WHERE sender = $1  ORDER BY id ASC', [userId], (error, results) => {
       if (error) {
         reject(error);
       }
-      if(results)
-            resolve(results.rows);
-          resolve([]);
+      if (results)
+        resolve(results.rows);
+      resolve([]);
     });
   })
 }
 
-async function getShipmentsByRecipientId (userId) {
+async function getShipmentsByRecipientId(userId) {
   console.log("db getShipmentsByRecipientId");
   return new Promise((resolve, reject) => {
-    pool.query('SELECT * FROM shipments WHERE recipient = $1 ORDER BY id ASC', [userId],  (error, results) => {
+    pool.query('SELECT * FROM shipments WHERE recipient = $1 ORDER BY id ASC', [userId], (error, results) => {
       if (error) {
         reject(error);
       }
-      if(results)
-            resolve(results.rows);
-          resolve([]);
+      if (results)
+        resolve(results.rows);
+      resolve([]);
     });
   })
 }
 
-async function getShipmentsByCarrierId (userId) {
+async function getShipmentsByCarrierId(userId) {
   console.log("db getShipmentsByCarrierId");
   return new Promise((resolve, reject) => {
-    pool.query('SELECT * FROM shipments WHERE carrier = $1 ORDER BY id ASC', [userId],  (error, results) => {
+    pool.query('SELECT * FROM shipments WHERE carrier = $1 ORDER BY id ASC', [userId], (error, results) => {
       if (error) {
         reject(error);
       }
-      if(results)
-            resolve(results.rows);
-          resolve([]);
+      if (results)
+        resolve(results.rows);
+      resolve([]);
     });
   })
 }
 
 
-async function findById (shipmentId) {
+async function findById(shipmentId) {
   console.log("db findShipmentByid")
   return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM shipments WHERE id = $1', [shipmentId], (error, results) => {
+    pool.query('SELECT * FROM shipments WHERE id = $1', [shipmentId], (error, results) => {
       if (error) {
         reject(error);
       }
-      if(results)
-            resolve(results.rows);
-          resolve();
+      if (results)
+        resolve(results.rows[0]);
+      resolve();
     })
   })
 }
 
-async function findByDeviceId (deviceId) {
+async function findByDeviceId(deviceId) {
   console.log("db findShipmentByDeviceid");
   return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM shipments WHERE device = $device', [deviceId], (error, results) => {
+    pool.query('SELECT * FROM shipments WHERE device = $device', [deviceId], (error, results) => {
       if (error) {
         reject(error);
       }
@@ -155,13 +155,13 @@ async function findByDeviceId (deviceId) {
   })
 }
 
-  module.exports = {
-    createShipment:createShipment,
-  getShipments:getShipments,
-  findById:findById,
-  getShipmentsBySenderId:getShipmentsBySenderId,
-  getShipmentsByRecipientId:getShipmentsByRecipientId,
-  getShipmentsByCarrierId:getShipmentsByCarrierId,
-  update:update,
-  findByDeviceId:findByDeviceId
-  };
+module.exports = {
+  createShipment: createShipment,
+  getShipments: getShipments,
+  findById: findById,
+  getShipmentsBySenderId: getShipmentsBySenderId,
+  getShipmentsByRecipientId: getShipmentsByRecipientId,
+  getShipmentsByCarrierId: getShipmentsByCarrierId,
+  update: update,
+  findByDeviceId: findByDeviceId
+};
