a216c3c332dd5e9368ca4f0a9258662684df3137
siem <siemarell@gmail.com>
2019-04-13 15:20:58 +0300

    deploy. createShipment route

--- frontend/src/components/CreateShipment/BasicInfo.tsx
+++ frontend/src/components/CreateShipment/BasicInfo.tsx
@@ -4,12 +4,14 @@ import ShipmentsStore from '@store/ShipmentsStore'
 
 @inject('shipmentsStore')
 @observer
-export default class BasicInfo extends React.Component<{shipmentsStore: ShipmentsStore}> {
+export default class BasicInfo extends React.Component<{shipmentsStore?: ShipmentsStore}> {
   render(){
     const shipmentCreation = this.props.shipmentsStore!.shipmentCreation
     return <div className='createShipment__basicInfo'>
       <div className='createShipment__basicInfo_text'>Basic information</div>
       <div className='createShipment__basicInfo_textDescription'>Carefully fill in all the fields to create a transportation</div>
+      <div className='createShipment__sender_text'>Sender</div>
+      <div className='createShipment__sender_textDescription'>Specify the place where the carrier should pick up the goodsSpecify the place where the carrier should pick up the goods</div>
     </div>
   }
 }
