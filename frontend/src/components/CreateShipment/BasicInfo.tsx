dcfe4f6ef7a3ccdb7c1b397c830e33fa6d94f4dd
Yuriy Naydenov <ebceu44@gmail.com>
2019-04-13 19:30:20 +0200

    *

--- frontend/src/components/CreateShipment/BasicInfo.tsx
+++ frontend/src/components/CreateShipment/BasicInfo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {inject, observer} from 'mobx-react'
+import { inject, observer } from 'mobx-react'
 import ShipmentsStore from '@store/ShipmentsStore'
 import DatePicker from 'react-datepicker'
 
@@ -17,11 +17,12 @@ export default class BasicInfo extends React.Component<{ shipmentsStore?: Shipme
         <div className='sender__text'>Sender</div>
         <div className='sender__textDescription'>Specify the place where the carrier should pick up the goods</div>
         <input className='location_input'
-               value={shipmentCreation.from}
-               onChange={(e) => shipmentCreation.from = e.target.value}
+          value={shipmentCreation.from}
+          onChange={(e) => shipmentCreation.from = e.target.value}
         />
         <div className='sender__textDescription'>Date</div>
         <DatePicker
+          className='__datePicker'
           selected={shipmentCreation.departureDate == null ? undefined : new Date(shipmentCreation.departureDate)}
           selectsStart
           // startDate={this.state.startDate}
@@ -33,16 +34,17 @@ export default class BasicInfo extends React.Component<{ shipmentsStore?: Shipme
         <div className='sender__text'>Recipient</div>
         <div className='sender__textDescription'>Recipient Key</div>
         <input className='recipient_input'
-               value={shipmentCreation.recipient}
-               onChange={(e) => shipmentCreation.recipient = e.target.value}
+          value={shipmentCreation.recipient}
+          onChange={(e) => shipmentCreation.recipient = e.target.value}
         />
         <div className='sender__textDescription'>Specify the place where the carrier should deliver the goods</div>
         <input className='location_input'
-               value={shipmentCreation.to}
-               onChange={(e) => shipmentCreation.to = e.target.value}
+          value={shipmentCreation.to}
+          onChange={(e) => shipmentCreation.to = e.target.value}
         />
         <div className='sender__textDescription'>Date</div>
         <DatePicker
+          className='__datePicker'
           selected={shipmentCreation.arrivalDate == null ? undefined : new Date(shipmentCreation.arrivalDate)}
           selectsStart
           // startDate={this.state.startDate}
--- frontend/src/components/Shipments/index.tsx
+++ frontend/src/components/Shipments/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import './styles.css'
 import StatusFilter from '@components/Shipments/StatusFilter'
 import OtherFilter from '@components/Shipments/OtherFilter'
-import {inject, observer} from 'mobx-react'
+import { inject, observer } from 'mobx-react'
 import ShipmentsStore from '@store/ShipmentsStore'
 import Shipment from '@components/Shipments/Shipment'
+import { Modal, Button } from 'react-bootstrap'
 
 @inject('shipmentsStore')
 @observer
 export default class Shipments extends React.Component<{ shipmentsStore?: ShipmentsStore }> {
 
+  state = {
+    show: false,
+  }
+
+  handleClose() {
+    this.setState({ show: false })
+  }
+
+  open() {
+    this.setState({ show: true })
+  }
+
   render() {
     const shipmentsStore = this.props.shipmentsStore!
     return <div className='shipments__root'>
       <div className='shipments__left'>
-        <StatusFilter/>
-        <OtherFilter/>
+        <StatusFilter />
+        <OtherFilter />
       </div>
       <div className='shipments__right'>
         <div className='shipments__right_topBar'>
@@ -25,13 +36,27 @@ export default class Shipments extends React.Component<{ shipmentsStore?: Shipme
             <div className='shipments__right_topBar_label'>Shipments</div>
             <div className='shipments__right_topBar_count'>Found {shipmentsStore.shipments.length} shipments</div>
           </div>
-          <div className='shipments__right_tobBar_addBtn'>+ Add Shipment</div>
+
+          <div className='shipments__right_tobBar_addBtn' onClick={() => {
+            this.open()
+          }}>+ Add Shipment</div>
         </div>
         <div className={'shipments__right_shipments'}>
-          {shipmentsStore.visibleShipments.map((shipment, i) => <Shipment key={i} shipment={shipment}/>)}
+          {shipmentsStore.visibleShipments.map((shipment, i) => <Shipment key={i} shipment={shipment} />)}
         </div>
       </div>
+      <Modal show={this.state.show} onHide={() => { this.handleClose() }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Transfer shipment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant='primary' onClick={() => { this.handleClose() }}>
+            Transfer
+            </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
+
   }
 }
-
