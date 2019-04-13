d8ad1bc9ddd507e01f21c0c238c8893c087a0036
Yuriy Naydenov <ebceu44@gmail.com>
2019-04-13 14:13:45 +0200

    logout

--- frontend/src/components/TopBar/index.tsx
+++ frontend/src/components/TopBar/index.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react'
 import './styles.css'
+import { observer, inject } from 'mobx-react'
+import AuthStore, { User } from '@src/store/AuthStore'
 
-const topBar = ({user}) => (<div className='topBar__root'>
-  <div className='topBar__logo'>Odyssey</div>
-  <div className='topBar__user'>{user.publicKey}</div>
-</div>)
+const trim = (str: string, len: number) => {
+  return str.substr(0, len) + '...'
+}
 
-export default topBar
+@inject('authStore')
+@observer
+export default class TopBar extends React.Component<{ authStore?: AuthStore, user: User }> {
+  render() {
+    const user = this.props.user
+    const authStore = this.props.authStore!
+    return (<div className='topBar__root'>
+      <div className='topBar__logo'>Odyssey</div>
+      <div className='topBar__user__card'>
+        <div>
+          <div className='topBar__userType'>{user.type}</div>
+          <div className='topBar__userKey'>{trim(user.publicKey, 30)}</div>
+        </div>
+        <img src='assets/icons/logout.svg' className='topBar__icon__logout' onClick={() => { authStore.logout() }}></img>
+      </div>
+
+    </div>)
+  }
+}
