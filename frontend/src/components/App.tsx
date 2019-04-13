14b5f775472f3cf17b7f3623644009dac052eba9
siem <siemarell@gmail.com>
2019-04-13 13:46:38 +0300

    top bar. filters

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,21 +2,34 @@ import React from 'react'
 import {inject, observer} from 'mobx-react'
 import AppStore from '../store/AppStore'
 import Shipments from '@components/Shipments'
+import TopBar from './TopBar'
 import './styles.css'
 
 interface IInjectedProps {
-    appStore?: AppStore
+  appStore?: AppStore
 }
+
 @inject('appStore')
 @observer
 export default class App extends React.Component<IInjectedProps> {
-    render(){
-        const appStore =  this.props.appStore!
-        return <div className='app_root'>
-            {/*Hello World!*/}
-            {/*{appStore.counter}*/}
-            {/*<button onClick={()=> appStore.counter++}>+</button>*/}
-            <Shipments/>
-        </div>
+  render() {
+    const logged = true;
+    const user = {
+      publicKey: 'abracadabra',
+      type: 'carrier',
     }
+    const appStore = this.props.appStore!
+    return <div className='app_root'>
+      {user ?
+        <><TopBar user={user}/>
+          <Shipments/></>
+        :
+        <div>Login</div>
+      }
+      {/*Hello World!*/}
+      {/*{appStore.counter}*/}
+      {/*<button onClick={()=> appStore.counter++}>+</button>*/}
+
+    </div>
+  }
 }
