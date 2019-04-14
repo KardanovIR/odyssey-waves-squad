623cc1a57264a854ee209b85d22075ec2a18bfa0
Yuriy Naydenov <ebceu44@gmail.com>
2019-04-14 09:13:36 +0200

    *

---
+++ frontend/src/components/TopBar/Shipments/StatusFilter.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { inject, observer } from 'mobx-react'
+import ShipmentsStore from '@src/store/ShipmentsStore'
+import InputGroup from 'react-bootstrap/InputGroup'
+import { statusLabelMap } from '@src/store/ShipmentsStore'
+import Up from '@src/icons/Up'
+
+interface IInjectedProps {
+  shipmentsStore?: ShipmentsStore
+}
+
+@inject('shipmentsStore')
+@observer
+export default class StatusFilter extends React.Component<IInjectedProps, { open: boolean }> {
+  state = {
+    open: true,
+  }
+
+  render() {
+    const shipmentsStore = this.props.shipmentsStore!
+
+    return <div>
+      <div className='statusFilter__button'
+        onClick={() => this.setState({ ...this.state, open: !this.state.open })}>Status
+        <Up className={'statusFilter__button_icon_' + (this.state.open ? 'up' : 'down')}/>
+        {/*<img src='assets/icons/up.svg' className={'statusFilter__button_icon_' + (this.state.open ? 'up' : 'down')}></img>*/}
+        {/* <img src='assets/icons/up.svg' className='statusFilter__button_icon_down'></img> */}
+      </div>
+      {this.state.open &&
+        <div className='statusFilter__checkboxes'>
+          {Object.entries(shipmentsStore.statusFilters).map(([filterKey, val]) => <InputGroup.Prepend key={filterKey}>
+            <InputGroup.Checkbox
+              checked={val}
+              onChange={() => shipmentsStore.statusFilters[filterKey] = !val}
+              className='statusFilter__checkbox'
+            />
+            <div className='statusFilter__checkbox_label'>{statusLabelMap[filterKey] || 'All'}</div>
+          </InputGroup.Prepend>)}
+          {/*{filters.map(filterTitle => <InputGroup.Prepend key={filterTitle}>*/}
+          {/*<InputGroup.Checkbox*/}
+          {/*checked={shipmentsStore.statusFilters[filterTitle]}*/}
+          {/*onChange={() => shipmentsStore.statusFilters[filterTitle] = !shipmentsStore.statusFilters[filterTitle]}*/}
+          {/*/>*/}
+          {/*<div className='statusFilter__checkbox_label'>{filterTitle}</div>*/}
+          {/*</InputGroup.Prepend>)}*/}
+        </div>
+      }
+    </div>
+  }
+}
